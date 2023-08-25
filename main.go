4a4c39a883d56edb99679b375c930ec167f5de43
Rahul Barna <97344601+RahulBarna00@users.noreply.github.com>
2023-08-25 19:46:27 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,75 @@
+// go mod init chatapp
+
+//go run main.go
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+
+type Message struct {
+	Username string `json:"username"`
+	Content  string `json:"content"`
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	clients[conn] = true
+
+	for {
+		var msg Message
+		err := conn.ReadJSON(&msg)
+		if err != nil {
+			fmt.Println(err)
+			delete(clients, conn)
+			break
+		}
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		msg := <-broadcast
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				fmt.Println(err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/ws", handleConnections)
+	go handleMessages()
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+
+	fmt.Println("Server started on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
